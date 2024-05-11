cecbabf3f511928a6b35269643c995df7c49b183
mazurinpstu2004 <mazurin2004@inbox.ru>
2024-05-11 15:28:50 +0500

    Задание по лабораторной работе № 6 "Шаблон проектирования PageObject"

--- src/test/java/PageObject/MicrosoftTest.java
+++ src/test/java/PageObject/MicrosoftTest.java
@@ -1,8 +1,6 @@
 package PageObject;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
